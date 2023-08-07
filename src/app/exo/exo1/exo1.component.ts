4ea3c03ba559b5a092c831f81d24958a6394017c
xLiZZaRRdd <yeansennerenaud@hotmail.com>
2023-08-07 16:56:02 +0200

    Exo01-Chronometre

--- src/app/exo/exo1/exo1.component.ts
+++ src/app/exo/exo1/exo1.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-exo1',
+  selector: 'app-chronometre',
   templateUrl: './exo1.component.html',
   styleUrls: ['./exo1.component.scss']
 })
-export class Exo1Component {
+export class Exo1Component implements OnInit, OnDestroy {
+  timerRunning: boolean = false;
+  timerValue: number = 0;
+  timerDisplay: string = '00:00';
+  timerSubscription: Subscription | undefined;
 
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  startTimer() {
+    this.timerRunning = true;
+    this.timerSubscription = interval(1000).subscribe(() => {
+      this.timerValue++;
+      this.updateTimerDisplay();
+    });
+  }
+
+  pauseTimer() {
+    this.timerRunning = false;
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  resetTimer() {
+    this.timerRunning = false;
+    this.timerValue = 0;
+    this.updateTimerDisplay();
+  }
+
+  updateTimerDisplay() {
+    const minutes = Math.floor((this.timerValue % 3600) / 60);
+    const seconds = this.timerValue % 60;
+
+    this.timerDisplay = this.formatTime(minutes) + ':' + this.formatTime(seconds);
+  }
+
+  formatTime(time: number): string {
+    return time < 10 ? '0' + time : time.toString();
+  }
 }
