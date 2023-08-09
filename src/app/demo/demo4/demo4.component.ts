c2a63081c80139f7294ca0cb4a271875e6dd3de4
xLiZZaRRdd <yeansennerenaud@hotmail.com>
2023-08-09 11:18:34 +0200

    Suite input&output

--- src/app/demo/demo4/demo4.component.ts
+++ src/app/demo/demo4/demo4.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo4.component.scss']
 })
 export class Demo4Component {
+  prenom : string = 'Robert';
+  besoinEnfant : string = '[Mon enfant n\'a besoin de rien]'
 
+  receiveNeed(besoin : string) : void
+  {
+    this.besoinEnfant = `Mon enfant a besoin de ${besoin}`;
+  }
 }
--- src/app/demo/demo4/enfant/enfant.component.ts
+++ src/app/demo/demo4/enfant/enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-enfant',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./enfant.component.scss']
 })
 export class EnfantComponent {
+  @Input() prenomParent : string = '[Pas de prénom]';
+  @Input() hobbiesParent : string[] = [];
 
+  //Vu que c'est un event, très souvent commencer par "on" le nommage
+  @Output() onNeedSelect : EventEmitter<string>;
+
+  constructor()
+  {
+    this.onNeedSelect = new EventEmitter<string>(); //Instanciation de EventEmitter
+  }
+
+  sendNeed(besoin : string)
+  {
+    this.onNeedSelect.emit(besoin);
+  }
 }
